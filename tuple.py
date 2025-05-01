0584e4d2005ae327adb0e604a8c23fd6731f1f31
Azaan Sarwar <xohansarwar@gmail.com>
2025-05-01 21:52:16 +0600

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,16 @@
+Fruits = ('apple', 'banana', 'cherry', 'date', 'elderberry', 'grape')
+
+print(Fruits[0])  
+print(Fruits[1])  
+print(Fruits[2])
+print(Fruits[3])
+print(Fruits[4])
+print(Fruits[5])
+print(Fruits[6])
+
+a = (1, 2)
+b = (3, 4)
+
+print(a + b)
+print(a * b)
+print(len(a))
